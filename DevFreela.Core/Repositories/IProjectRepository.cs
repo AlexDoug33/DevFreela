61479774c6c92bbc98b9eccf17f896fde4ac0f9d
Alex  Douglas <alex.almeida@alfaerp.com.br>
2023-10-25 09:14:30 -0400

    First Deploy

---
+++ DevFreela.Core/Repositories/IProjectRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevFreela.Core.Repositories
+{
+    public interface IProjectRepository
+    {
+    }
+}
+
+
+
+
+
